2640a16f6751baa84b9c9423779b56321f01be69
Filip Claesson <filipclaesson@Filips-MacBook-Air.local>
2020-01-24 08:05:03 +0100

    updated report lab api

--- reportlab_api.py
+++ reportlab_api.py
@@ -4,9 +4,9 @@
 
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import cm
-from reportlab.platypus import Frame,Table,Paragraph
+from reportlab.platypus import Frame,Table,Paragraph,Image,TableStyle,Spacer
 from reportlab.lib.colors import pink, black, red, blue, green,white
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet,ParagraphStyle
 from reportlab.lib.colors import Color
 from reportlab.lib.utils import ImageReader
 import numpy as np
@@ -15,12 +15,13 @@ from reportlab.pdfgen.canvas import Canvas
 
 
 
+
 class Document:
 
     def __init__(self, name, pagesize=defaultPageSize):
         self.pagesize=pagesize
         self.name=name
-        self.canvas = Canvas('mydoc.pdf', pagesize=self.pagesize)
+        self.canvas = Canvas(name, pagesize=self.pagesize)
         self.pages = []
 
 
@@ -38,6 +39,7 @@ class Document:
             page.draw_page_elements()
             for row in page.get_rows():
                 for column in row.get_columns():
+                    #print('drawing ' + column.name)
                     column.draw_elements()
             print('page ' + str(page.page_number) + ' complete')
             self.canvas.showPage()
@@ -58,7 +60,7 @@ class Page:
    
     """
     # Initializer / Instance Attributes
-    def __init__(self, name,page_number, document,top_padding=2,left_padding=2, background_path=None):
+    def __init__(self, name,page_number, document,y_padding=2,x_padding=2, background_path=None):
         self.name = name
         self.page_number = page_number
         self.rows = []
@@ -67,18 +69,15 @@ class Page:
 
         self.document.add_page(self)
         
+        self.y_padding=y_padding*cm
+        self.x_padding=x_padding*cm
 
-        self.top_padding=top_padding*cm
-        self.left_padding=left_padding*cm
-
-        self.logo_path = None
-        self.logo_settings = {}
-
+        self.logo = None
         self.background_path=background_path
 
     def get_y_pos_for_level(self,level):
         height_sum = sum([r.get_height() for r in self.rows[:level]]) # sum all 
-        return (self.pagesize[1]-self.top_padding)-height_sum
+        return (self.pagesize[1]-self.y_padding)-height_sum
     
     ## TODO: error handling, should not be able to add more rows than max pange height   
     def add_row(self,row):
@@ -96,23 +95,15 @@ class Page:
     def get_document(self):
         return(self.document)
 
-    def draw_page_elements(self):
-        # draw logo
-        if self.logo_path is not None:
-            logo = ImageReader(self.logo_path)
-            if len(self.logo_settings) == 0:
-                self.document.get_canvas().drawImage(logo, 10, 10,mask='auto')
-            else:
-                if self.logo_settings['position']=='bottom-right':
-                    y = self.logo_settings['padding']
-                    x = float(self.pagesize[0])-logo.getSize()[0]-self.logo_settings['padding']
-                    print('x: ' + str(x) + ', y: ' + str(y))
-                    self.document.get_canvas().drawImage(logo, y=y, x=x,mask='auto')
+    def get_padding(self):
+        return {'x_padding':self.x_padding,'y_padding':self.y_padding}
 
-    def add_logo(self,logo_path,position,padding):
-        self.logo_path = logo_path
-        self.logo_settings['position'] = position
-        self.logo_settings['padding'] = padding*cm
+    def draw_page_elements(self):
+        if self.logo is not None:
+            self.logo.draw()
+
+    def add_logo(self,logo):
+        self.logo = logo
 
 
    
@@ -151,7 +142,7 @@ class Row:
     def get_x_pos_for_level(self,level):
         width_sum = sum([c.get_width() for c in self.columns[:level]]) # sum all 
         # max_width = self.get_page().pagesize[0]
-        return self.get_page().left_padding + width_sum
+        return self.get_page().x_padding + width_sum
 
     def get_page(self):
         return self.page
@@ -166,60 +157,120 @@ class Row:
 
 class Column:
 
-    def __init__(self,name,width,level,row):
+    def __init__(self,name,width,level,row,show_boundry=0,padding_left=0):
         self.name = name
         self.width = width*cm
         self.level = level
         self.row = row
         self.elements = []
+        self.padding_left = padding_left*cm
+        
 
         # add the row to the page
         self.row.add_column(self)
         # get the y pos from the page for the current level
         self.x_pos=self.row.get_x_pos_for_level(level) - self.width
+        self.y_pos=self.row.get_y_pos() 
+        #print('init column ' + self.name + ', x_pos: ' + str(self.x_pos) + ', y_pos: ' + str(self.y_pos))
 
         self.frame = Frame(
             x1=self.x_pos, y1=self.row.get_y_pos(),
             width=self.width,height=self.row.get_height(),
             leftPadding=0, bottomPadding=0,
             rightPadding=0, topPadding=6,
-            showBoundary=0
+            showBoundary=show_boundry
         )
 
     def get_width(self):
-        return self.width
+        return self.width+self.padding_left
 
     def get_row(self):
         return self.row
+
+    def get_frame(self):
+        return self.frame
 
     def add_element(self,element):
         self.elements.append(element)
 
     def draw_elements(self):
-        self.frame.addFromList(self.elements, self.get_row().get_page().get_document().get_canvas())
+        for e in self.elements:
+            e.draw()
 
 
 
 
-## Separate container for setting up elements
-class ComponentHelper():
-
-    def __init__(self):
-        self.styles = getSampleStyleSheet()
 
 
-    def setup_paragraph(self,text,settings):
-        from reportlab.lib.styles import ParagraphStyle
+
+
+########################################
+##                                    ##
+##                                    ##
+##              Components            ##
+##                                    ##
+##                                    ##
+########################################
+class Component:
+    def __init__(self, name):
+        self.name=name
+
+
+
+class ColumnComponent(Component):
+    def __init__(self, name, column):
+        super().__init__(name)
+        self.column = column
+        self.column.add_element(self)
+
+
+    def draw(self):
+        # NOTE THAT THE self.platypus_object is defined in the children
+        self.column.get_frame().addFromList([self.platypus_object], self.column.get_row().get_page().get_document().get_canvas())
+
+class ColImage(ColumnComponent):
+    def __init__(self,name,column,path,width=None,height=None):
+        super().__init__(name,column)
+        img = ImageReader(path)
+        iw, ih = img.getSize()
+        aspect = ih / float(iw)
+        if width is None:
+            width = aspect*2
+        if height is None:
+            height=(width * aspect)
+        
+        self.platypus_object = Image(path, width=width, height=height, hAlign='LEFT')
+
+class ColParagraph(ColumnComponent):
+    
+    def __init__(self,name,column,text,text_settings):
+        super().__init__(name,column)
         style = ParagraphStyle('test')
-        for key in settings:
-            style.__dict__[key] = settings[key]
-        text_style_added = text
-        p = Paragraph(text, style)
-        return p
+        for key in text_settings:
+            style.__dict__[key] = text_settings[key]
+        self.platypus_object = Paragraph(text,style)
+
+class ColSpacer(ColumnComponent):
+    
+    def __init__(self,name,column,x,y):
+        super().__init__(name,column)
+        self.platypus_object = Spacer(x,y)
+
+class ColTable(ColumnComponent):
+
+    def __init__(self,name,column,df,totals_line=False,altering_colors=False,red_negative_col=None,formating=[]):
+        super().__init__(name,column)
+        self.setup_table(df,totals_line,altering_colors,red_negative_col,formating)
+        
+        table = Table(self.df);
+        table.setStyle(TableStyle(self.style))
+
+        self.platypus_object = table
 
 
     def setup_table(self, df,totals_line=False,altering_colors=False,red_negative_col=None,formating=[]):
         df_list = np.insert(np.array(df).tolist(), 0, np.array(df.columns.values), axis=0)
+        df_list = df_list.astype('U140', copy=False)
         tot_cols = df.shape[1]-1
         style=[]
         style.append(('TEXTCOLOR',(0,0),(tot_cols,0),black))
@@ -254,24 +305,67 @@ class ComponentHelper():
             for idx, val in (df.iterrows()):
                 if(val[red_negative_col-1]<0):
                     style.append(('TEXTCOLOR',(0,idx+1),(tot_cols,idx+1),red))
-        
+        #style.append(('ALIGN', (1,1), (-1,-1), 'RIGHT'))
         ## formating
         for inx,l in enumerate(df_list):
             if inx == 0:
                 pass
             else:
                 for col,format_ in enumerate(formating):
+                    
                     if format_ == 'kr':
-                        df_list[inx][col] = str(int(float(df_list[inx][col])))+' kr '
+                        df_list[inx][col] = str(int(float(df_list[inx][col])))+' kr'
+                        
+                    
+
                     if format_ == '':
                         pass
+
+
+        self.df = df_list.tolist()
+        self.style = style
         return [style,df_list.tolist()]
 
-
-## Component helpters
- class Logo:
-
     
+        
+## Logo är inte en del av columnerena utan ritas separat
+class Logo(Component):
+    def __init__(self, name, path, page, width, height, position, padding):
+        super().__init__(name)
+        self.path = path
+        self.page = page
+        self.width = width
+        self.height = height
+        self.padding = padding*cm
+        self._set_postition_from_text(position)
+        self.page.add_logo(self)
+
+    def draw(self):
+        self.page.get_document().get_canvas().drawImage(self.path, self.x, self.y,mask='auto')
+
+    def _set_postition_from_text(self, position_text):
+        if position_text.split('-')[0]=='bottom':
+            self.y = self.padding
+        if position_text.split('-')[0]=='top':
+            self.y = self.page.get_document().get_pagesize()[1]- self.padding - self.width
+        if position_text.split('-')[1]=='left':
+            self.x = self.padding
+        if position_text.split('-')[1]=='right':
+            self.x = self.page.get_document().get_pagesize()[0]-self.width-self.padding
+
+        #print('x: ' + str(self.x) + ', y: ' + str(self.y))
+
+
+########################################
+##                                    ##
+##                                    ##
+##           How to use it            ##
+##                                    ##
+##                                    ##
+########################################
+
+
+
 
 
 ## Example of how to use it
@@ -283,8 +377,8 @@ class ComponentHelper():
 # 
 # p = rapi.Page("page1",1
 #       ,document=d
-#       ,top_padding=1.5
-#       ,left_padding=4
+#       ,y_padding=1.5
+#       ,x_padding=4
 #      )
 # r1 = rapi.Row(name="header1",height=7,level=1,page=p)
 # r1_c1 = rapi.Column(name="r1c1",width=10,level=1,row=r1)
@@ -310,8 +404,8 @@ class ComponentHelper():
 # for page in range(0,math.ceil(len(plots)/4)):
 #     p = rapi.Page("page1",page
 #           ,document=d
-#           ,top_padding=1.5
-#           ,left_padding=2
+#           ,y_padding=1.5
+#           ,x_padding=2
 #          )
 #     r1 = rapi.Row(name="header1",height=7,level=1,page=p)
 #     r1_c1 = rapi.Column(name="r1c1",width=10,level=1,row=r1)
